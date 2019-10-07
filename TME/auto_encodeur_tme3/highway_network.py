c97ea745fe91cd700612a93b8339d6e1b82403c0
marc-treu <mr.treu@gmail.com>
2019-10-07 11:44:09 +0200

    Implementation du highway

---
+++ TME/auto_encodeur_tme3/highway_network.py
@@ -0,0 +1,18 @@
+import torch
+
+
+class Highway(torch.nn.Module):
+
+    def __init__(self, d_in):
+
+        self.linear = torch.nn.Linear(d_in, d_in)
+        self.transform = torch.nn.Linear(d_in, d_in)
+
+        self.H = torch.nn.ReLU()
+        self.T = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        
+        gate = self.T(self.transform(x))
+
+        return torch.matmul(self.H(self.linear(x)),gate) + torch.matmul(1 - gate, x)
